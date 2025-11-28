7de67cb9d80d412f2f79473ef1ee17bb49602730
9uiLe <kobappleple@gmail.com>
2025-11-29 01:49:00 +0900

    fix

--- Package.swift
+++ Package.swift
@@ -2,102 +2,24 @@
 
 import PackageDescription
 
-// MARK: - Plugin
-
-let runMockoloPlugin = Target.PluginUsage.plugin(
-    name: "RunMockolo",
-    package: "run-mockolo"
-)
-
-// MARK: - Core
-
-let imageLoader = Target.target(
-    name: "ImageLoader",
-    plugins: [
-        runMockoloPlugin
-    ]
-)
-
-// MARK: - Package
-
-let package = Package.package(
+let package = Package(
     name: "image-loader",
     platforms: [
         .iOS(.v18)
+    ],
+    products: [
+        .library(name: "ImageLoader", targets: ["ImageLoader"])
     ],
     dependencies: [
         .package(url: "https://github.com/9uiLe/run-mockolo", exact: "1.0.2")
     ],
     targets: [
-        imageLoader
-    ],
-    testTargets: []
-)
-
-// MARK: - Target Extension
-
-extension Target {
-    private var dependency: Target.Dependency {
-        .target(name: name, condition: nil)
-    }
-
-    fileprivate func library(targets: [Target] = []) -> Product {
-        .library(name: name, targets: [name] + targets.map(\.name))
-    }
-
-    static func target(
-        name: String,
-        path: String = "Sources/",
-        dependencies: [Target] = [],
-        dependencyLibraries: [Target.Dependency] = [],
-        resources: [Resource] = [],
-        plugins: [Target.PluginUsage] = [],
-        swiftLanguageMode: SwiftLanguageMode = .v6
-    ) -> Target {
         .target(
-            name: name,
-            dependencies: dependencies.map(\.dependency) + dependencyLibraries,
-            path: path + name,
-            resources: resources,
-            swiftSettings: [
-                .swiftLanguageMode(swiftLanguageMode)
-            ],
-            plugins: plugins
-        )
-    }
-
-    static func testTarget(
-        name: String,
-        dependencies: [Target],
-        swiftLanguageMode: SwiftLanguageMode = .v6
-    ) -> Target {
-        .testTarget(
-            name: name,
-            dependencies: dependencies.map(\.dependency),
-            swiftSettings: [
-                .swiftLanguageMode(swiftLanguageMode)
+            name: "ImageLoader",
+            dependencies: [
+                .product(name: "RunMockolo", package: "run-mockolo")
             ]
         )
-    }
-}
-
-// MARK: - Package Extension
-
-extension Package {
-    static func package(
-        name: String,
-        platforms: [SupportedPlatform],
-        dependencies: [Dependency] = [],
-        targets: [Target],
-        testTargets: [Target]
-    ) -> Package {
-        Package(
-            name: name,
-            defaultLocalization: "ja",
-            platforms: platforms,
-            products: targets.map { $0.library() },
-            dependencies: dependencies,
-            targets: targets + testTargets
-        )
-    }
-}
+    ],
+    swiftLanguageModes: [.v6]
+)
